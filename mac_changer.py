a1580479444aa79966060ddc4c2b3fa809c3c2bc
AbdullahRizwan101 <60057481+AbdullahRizwan101@users.noreply.github.com>
2020-04-27 09:18:23 -0400

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,45 @@
+import subprocess
+import optparse
+import re
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="To select an interface")
+    parser.add_option("-m","--mac",dest="mac",help="Set MAC address of an interface")
+    (value,arg) = parser.parse_args()
+    if not value.interface or not value.mac:
+        print("[-] please specifiy options properly ,use --help for info")
+    else:
+        return value 
+       
+
+def change_mac(interface,mac):
+    print("[+] Changing MAC for "+interface+" To"+" "+mac)
+    subprocess.call(["ifconfig",interface,"down"])
+    subprocess.call(["ifconfig",interface,"hw","ether",mac])
+    subprocess.call(["ifconfig",interface,"up"])
+
+def current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig",values.interface])
+    search_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",ifconfig_result)
+    if search_mac:
+       return search_mac.group(0)
+    else:
+        print("[-] Cannot Find MAC Address ")    
+
+values = get_args()
+curr_mac = current_mac(values.interface)
+print("[+] Current MAC Address is : "+str(curr_mac))
+change_mac(values.interface,values.mac)
+curr_mac = current_mac(values.interface)
+
+if curr_mac == values.mac:
+    print("[+] Mac address successfully changed to : "+str(curr_mac))
+
+
+
+    
+
+
+
+
---
+++ network_scanner.py
@@ -0,0 +1,40 @@
+import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t","--target",dest="target",help="Specify an IP address or the whole subnet")
+    value = parser.parse_args()
+
+    if not value.target:
+        print("[-] Please specfiy an IP or Subnet")
+    else:    
+        return value
+
+def scan(ip):
+    # message for ARP request for asking IP address
+    arp_request = scapy.ARP(pdst=ip) 
+
+    #sending boradcast packet for MAC address
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") 
+
+    #combinig ARP & MAC boradcast packet 
+    arp_request_broadcast = broadcast/arp_request
+
+    # boradcasting packet and saving only answered responses
+    answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+    client_list = [] # Making List of scanned clients
+    for element in answered_list:
+        client_dict = {'ip':element[1].psrc,'mac':element[1].hwsrc} #dictionary with scanned ip & MAC of clients
+        client_list.append(client_dict) # adding dictionary to list
+    return client_list # returning client list with scanned dictionary of IP & MAC
+
+def print_result(captured_list):
+        # iterating through the answered list and only printing IP & MAC addresses
+        print('IP\t\t\tMAC\n----------------------------------')
+        for client in captured_list:
+            print(client["ip"]+"\t\t"+client["mac"]) # printing clients IP & MAC
+
+target_value = get_arguments()
+scan_result = scan(target_value.target)
+print_result(scan_result)
